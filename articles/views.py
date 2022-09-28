35c48ec5799a7c9db10ca8803d7bb7c0b7401cc6
leehyewon <tnddl2345@gmail.com>
2022-09-28 22:34:48 +0900

    init project d4

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article   # 같은 폴더 내에 있는 파일이기 때문에
 
 # Create your views here.
 
@@ -17,10 +18,12 @@ def dinner(request, name):
     ]
 
     pick = random.choice(menus)
+    articles = Article.objects.order_by('-pk')
     context = {
         'pick' : pick,
         'name' : name,
         'menus' : menus,
+        'articles' : articles
     }
     return render(request, 'dinner.html', context)
 
@@ -31,8 +34,8 @@ def review(request):
 
 def create_review(request):
     content = request.POST.get('content')
-    print(request.POST)
-    context = {
-        'content' : content,
-    }
-    return render(request, 'review_result.html', context)
+    title = request.POST.get('title')
+    article = Article(title=title, content=content)
+    article.save()
+
+    return redirect('/articles/dinner/list')
