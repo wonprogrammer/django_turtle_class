ea19a32de212b213232a3e574c2d8780be88da81
leehyewon <tnddl2345@gmail.com>
2022-09-28 12:04:50 +0900

    init project d4

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
+from turtle import title
+from unittest.util import _MAX_LENGTH
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+class Articles(models.Model):  # 상속
+    # id는 자동으로 만들어짐
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    
